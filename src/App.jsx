cb40f12e476d7b04920981bf818845ff01e28202
hagermahmoudgad-create <hagermahmoudgad@gmail.com>
2025-11-15 20:33:03 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProfileCard from "./ProfileCard";
-import { UserProvider } from "./UserContext";
+
 
 function App() {
 
